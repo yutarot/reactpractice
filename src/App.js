de96c55682dae62c0aa7fac83f6c10d1a6ffa379
yutarot <takenoco88@gmail.com>
2018-05-12 18:03:47 +0900

    considering page structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
+import styles from './App.css'
 
-const ButtonExampleButton = () => (
-  <Button>Click Here</Button>
+const ContainerExampleText = () => (
+  <div className={styles.hoge}>
+    <Container className='' text>
+      <Header as='h1'>Header</Header>
+    </Container>
+    <Container text>
+      <div>content here.</div>
+    </Container>
+    <Container text>
+      <span>footer</span>
+    </Container>
+  </div>
 )
 
-export default ButtonExampleButton
+export default ContainerExampleText
